a8c3b2f78f01ac542a678ddc4bfedd2f213e930c
Mathieu Parisot <mathieu@medici.md>
2017-11-19 16:26:45 +0100

    Switch between sections

--- website/src/main.js
+++ website/src/main.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import cc from 'classcat';
 
 import SearchSection from './components/search/SearchSection';
 
 import '../styles/style.css';
 
+
 class Main extends React.Component {
 
     state = {
+        sectionDisplayed: 'search',
+    };
+
+    switchSection = () => { // add a proper router when we have more than 2 sections
+        this.setState({
+            sectionDisplayed: this.state.sectionDisplayed === 'search' ? 'addMovie' : 'search',
+        });
     };
 
     render() {
         return (<div className="main">
-            <SearchSection />
+            <button className="sectionSwitch" onClick={this.switchSection}>{this.state.sectionDisplayed === 'search' ? 'Add new movie' : 'Go back to search'}</button>
+            <div className={`sections ${this.state.sectionDisplayed}`}>
+                <SearchSection />
+                <div className="addMovieForm">Add a movie</div>
+            </div>
         </div>);
     }
 }
