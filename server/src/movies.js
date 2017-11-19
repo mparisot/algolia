e111552de42719179013d7fedcd4d2451e329972
Mathieu Parisot <mathieu@medici.md>
2017-11-19 19:51:17 +0100

    code reformat

--- server/src/movies.js
+++ server/src/movies.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.route('/movies/:movieId').delete(function (req, res) {
     console.log('Delete movie', req.params.movieId);
-    movieManager.delete(req.params.movieId).then(() => res.send({delete: true})).catch(err => res.status(500).send(err.message));
+    movieManager.delete(req.params.movieId)
+        .then(() => res.send({delete: true}))
+        .catch(err => res.status(500).send(err.message));
 
 });
 
