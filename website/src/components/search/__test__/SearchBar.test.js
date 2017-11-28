75c12aaf244a5f369f26f398d5cfbc69b73251fe
Mathieu Parisot <mathieu@medici.md>
2017-11-28 10:53:51 +0100

    Add test when the user remove their input in search bar

--- website/src/components/search/__test__/SearchBar.test.js
+++ website/src/components/search/__test__/SearchBar.test.js
@@ -38,6 +38,38 @@ test('SearchBar update results when search yield results', (done) => {
     },1);
 });
 
+test('SearchBar deleting what we searched for should return empty results', (done) => {
+    const mockSearchResultUpdate = jest.fn();
+
+    const searchBar = mount(<SearchBar onSearchResultsUpdated={mockSearchResultUpdate}/>);
+
+    const input = searchBar.find('input');
+    input.simulate('change', { target: { value: 'text' } });
+
+    setTimeout(() => {
+        input.simulate('change', { target: { value: '' } });
+
+        setTimeout(() => {
+            try {
+                expect(mockSearchResultUpdate.mock.calls.length).toBe(2);
+
+                const firstCallResult = mockSearchResultUpdate.mock.calls[0][0];
+                expect(Array.isArray(firstCallResult)).toBe(true);
+                expect(firstCallResult.length).toBe(1);
+                expect(firstCallResult[0].objectID).toBe('1');
+                expect(firstCallResult[0].title).toBe('title');
+
+                const secondCallResult = mockSearchResultUpdate.mock.calls[1][0];
+                expect(Array.isArray(secondCallResult)).toBe(true);
+                expect(secondCallResult.length).toBe(0);
+                done();
+            } catch(error) {
+                done.fail(error);
+            }
+        },1);
+    }, 1);
+});
+
 test('SearchBar display error message when search fails', done => {
     const searchBar = shallow(<SearchBar onSearchResultsUpdated={(result) => null}/>);
 
